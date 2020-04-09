be05912b45b9f316683d508069e3bb8c14127084
Melih Sinan Dogrul <melihsinan.dogrul@i2isystems.local>
2020-04-10 00:12:33 +0300

    initial commit

---
+++ public/sketch.js
@@ -0,0 +1,98 @@
+var socket;
+var input;
+var roomId;
+var hide = false;
+var taboo ;
+var arrow ; 
+
+
+function setup() {
+    let cnv = createCanvas(windowWidth /4 * 3, windowHeight / 3 * 2);
+    cnv.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2 - 50);
+    socket = io.connect('http://localhost:80');
+    textFont('Georgia');
+    socket.on('receiveWord', receiveWord);
+
+    taboo = new Taboo();
+    arrow = new Arrow();    
+
+    roomInput = createInput();
+    console.log()
+    
+    roomInput.position(cnv.position().x + width/2 - roomInput.width/2 , cnv.position().y + height );
+
+
+
+}
+
+function mouseClicked(event) {
+    if (mouseX < width && mouseX > width / 3 * 2) {
+        requestWord();
+    }else if(mouseY < height && mouseY > height / 3 * 2){
+        joinRoom();
+    }else if(mouseX > 0 && mouseX < width / 3){
+        hide = !hide;
+    }
+}
+
+function draw() {
+    background(0);
+    taboo.show();
+    arrow.show();
+}
+
+function joinRoom() {
+    roomId = roomInput.value();
+    socket.emit('joinRoom', roomId);
+    requestWord();
+}
+
+function requestWord() {
+    console.log("word requested .. ");
+    socket.emit('requestWord', roomId);
+}
+
+function receiveWord(word) {
+    taboo.word = word;
+}
+
+function Arrow() {
+    this.x = width - width / 6 + 20;
+    this.y = height / 2
+    this.show = function () {
+        triangle(this.x, this.y - 20, this.x, this.y + 20, this.x + 20, this.y);
+        textSize(30);
+        text("JOIN ROOM",width/2, height- 40);
+        ellipse(80, height/2, 40, 40);
+    }
+}
+
+
+function Taboo() {
+    this.word = { keyword: "word", taboo_words: ["taboo", "taboo1", "taboo2"] }
+    this.show = function () {
+        if(!hide){
+            textAlign(CENTER);
+            textSize(60);
+            fill(255, 255, 255);
+            var textPosition = 70;
+    
+            text(this.word.keyword.toUpperCase(), width / 2, textPosition);
+            textPosition += 30;
+    
+            this.word.taboo_words.forEach(taboo => {
+                textPosition += 70;
+                text(taboo.toUpperCase(), width / 2, textPosition);
+            });
+            
+            stroke(255)
+            strokeWeight(12.0);
+            line(0, 100, width, 100)
+            strokeWeight(2.0);
+        }
+    }
+
+    this.update = function () {
+
+    }
+}
---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000
+var server = app.listen(port);
+app.use(express.static('public'));
+
+var socket = require('socket.io');
+var io = socket(server);
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('wordsTR.json');
+var wordList = JSON.parse(rawdata);
+var words = wordList.words;
+
+
+io.sockets.on('connection', newConnection);
+
+
+function newConnection(socket) {
+    console.log("new connection : " + socket.id);
+
+    socket.on('joinRoom', joinRoom);
+    socket.on('requestWord', requestWord);
+
+    function joinRoom(roomId) {
+        if(roomId){
+            console.log("socket : " + socket.id + " joining : " + roomId);       
+        
+            var rooms = Object.keys(io.sockets.adapter.sids[socket.id]);
+            rooms.splice(rooms.indexOf(socket.id),1);
+            rooms.splice(rooms.indexOf(roomId),1);
+
+            rooms.forEach(function(room) {
+                console.log("socket : " + socket.id + " Leaving from : " + room)
+                socket.leave(room);
+            });
+            
+            socket.join(roomId);
+    
+            var roomsUpdated = Object.keys(io.sockets.adapter.sids[socket.id]);
+            console.log("rooms now : " + roomsUpdated);
+        }
+
+    }
+
+    function leaveOthers() {
+
+    }
+
+    function requestWord(roomId) {
+        console.log("sending word ... ");
+        rndm = Math.floor(Math.random() * 800);
+        let word = words[rndm];
+        console.log("word : " + word + " random : " + rndm);
+        io.sockets.to(roomId).emit('receiveWord', word);
+    }
+
+}
+
+function clientsInRoom(roomId) {
+    var clients = io.sockets.adapter.rooms[roomId].sockets;
+    for (client in clients){
+        console.log("member : " + client)
+    }
+}
+
+
+console.log("socket is running ...."); 
+
